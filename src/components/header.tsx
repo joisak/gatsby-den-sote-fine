193a6bf5e58ae4e4785da4abf22db63c9974796f
Joakim Isaksson <joakim.isaksson@spinit.se>
2020-11-18 14:23:36 +0100

    testing header stuff

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,14 +20,21 @@ const Header = () => {
     <>
     {headerMenu && 
       <div className="header-menu">
+        <div className="left">
         { headerMenu.map((item, index) => {
-          if(index <= 1) {
-            return (<div className="left"><Link to={item.link_path}>{item.link_title}</Link></div>)
-          } else {
-            return (<div className="right"><Link to={item.link_path}>{item.link_title}</Link></div>)
-          }
-         
+          if(index <= 2)
+            return (<Link to={item.link_path}>{item.link_title}</Link>)
         }) }
+        </div>
+        <div className="logo">
+          LOGO
+        </div>
+        <div className="right">
+        { headerMenu.map((item, index) => {
+          if(index >= 2)
+            return (<Link to={item.link_path}>{item.link_title}</Link>)
+        }) }
+        </div>
         
         {data.allSettingsYaml.nodes[0].header_navigation.forEach(item => {
           {console.log(item.link_path)}
